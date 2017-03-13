59b9a178ed65524a3ea6a0b8e8bc80c3ac20bd27
Ludovic Lasry <llasry@ludo-mac.home>
2017-03-13 22:45:13 +0100

    add socketiomiddleware

---
+++ src/lib/socketIOMiddleware.js
@@ -0,0 +1,95 @@
+const TYPE_ERROR = 'error';
+const COMMAND_UPDATE_USR_LIST = 'update_user_list';
+
+function sendText(socket,type,message) {
+  // Construct a msg object containing the data the server needs to process the message from the chat client.
+  var msg = {
+    text: message,
+    date: Date.now()
+  };
+
+  // Send the msg object as a JSON-formatted string.
+  socket.emit(type,JSON.stringify(msg));
+}
+
+var socket = null;
+
+export default function socketIOMiddleware() {
+  return next => action => {
+
+  	const { type, ...rest } = action;
+  	console.log("websock " + action.type);
+
+  	//if the action type is not handle by this middleware
+  	if(action.type !== "CREATE_SOCKET" && action.type !== "SEND_MESSAGE" && action.type !== "DISCONNECT_SOCKET")
+  		return next(action);
+
+
+    if(action.type === 'CREATE_SOCKET')
+      socket = require('socket.io-client')("http://localhost:8100");
+
+    if(action.type === "SEND_MESSAGE" && socket)
+    {
+      sendText(socket,action.messageType,action.message);
+      return next(action);
+    }
+
+    if(action.type === "DISCONNECT_SOCKET")
+      socket.close();
+  	 
+
+  	 socket.on("connection",(e) => {
+      // connection opened
+      console.log("connexion opened");
+      /*if(wainting_messages.length >0)
+        for(index in wainting_messages)
+        {
+          socket.send(wainting_messages[index]);
+        } */
+        sendText(socket,"init",action.login,action.channel);
+        return next({ ...rest, type: "SOCKET_OPENED", socket:socket , login:action.login});
+    });
+
+     socket.on(COMMAND_UPDATE_USR_LIST,(e)=>{
+
+
+     });
+
+
+   socket.on("message",(e) => {
+      // a message was received
+      let message = JSON.parse(e.data);
+      console.log("message reçu ");
+      console.log(message);
+      let type = "SOCKET_MESSAGE_RECEIVED";
+      if(message.command !== false)
+        type = "SOCKET_COMMAND_RECEIVED"
+
+      return next({ ...rest, type: type , message:message });//Gérer le cas d un message array pour transmettre une nouvelle action pour gérer la liste user par ex
+    });
+
+   socket.on(TYPE_ERROR,(e) => {
+      let message = e.data;
+      console.log("error " + e.message);
+      return next({ ...rest, type: "SOCKET_ERROR_MESSAGE", message:message });
+   });
+
+    socket.onerror = (e) => {
+      // an error occurred
+      console.log("error " + e.message);
+      return next({ ...rest, type: "SOCKET_ERROR_MESSAGE", message:"connexion error" });
+    };
+
+   socket.onclose = (e) => {
+      // connection closed
+      let message = e.code + ":" + e.reason;
+      console.log("fermeture : " + e.code, e.reason);
+      return next({ ...rest, type: "SOCKET_CLOSED", message:message });
+    };
+
+    /*console.log(socket);
+    return socket;*/
+
+  }
+
+}
